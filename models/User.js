6a5c65855b2481e9333795c9de22a6d5f3aff5b7
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-30 01:24:58 -0600

    added user model

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+//user model
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            alowNull: false
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        }
+    },
+    {
+        hooks: {
+            //set up password safety
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //do the same for password update
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password,10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+)
+
+module.exports = User;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,97 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+//routes for getting, creating, deleting, and updating users
+
+router.get('/', (req,res)=>{
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+    .then(dbUserData=>{
+        res.json(dbUserData);
+    })
+    .catch(errr=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req,res)=>{
+    User.findOne({
+        attributes: {exclude: ['password']},
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData=>{
+        if(!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req,res)=>{
+    //expects: {username: xxxx, first_name: xxxx, last_name: xxxx, email: xxxx@xxx.com, password: xxxxxx}
+    User.create({
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData=>{
+        res.json(dbUserData);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req,res)=>{
+    //expects the same as creation
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData=>{
+        if(!dbUserData[0]){
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req,res)=>{
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData=>{
+        if(!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
